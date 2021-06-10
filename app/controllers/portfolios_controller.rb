bd4a6fdb33d3708a4eddef51fccc19f1bcb91fcb
alisuhaib1512 <alisuhaib88@gmail.com>
2021-06-10 20:34:06 +0500

    Ability to edit databse items from the scratch

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -11,8 +11,7 @@ class PortfoliosController < ApplicationController
   end
 
   def create
-    @portfolios_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
-
+    @portfolios_item = Portfolio.new(portfolio_params)
     respond_to do |format|
       if @portfolios_item.save
         format.html { redirect_to portfolios_path, notice: "Portfolio is live now." }
@@ -23,7 +22,30 @@ class PortfoliosController < ApplicationController
       end
     end
   end
-   def portfolia_params
+
+  def portfolio_params
       params.require(:portfolio).permit(:title, :subtitle, :body)
+  end
+
+  def edit
+      @portfolios_item = Portfolio.find(params[:id])
+  end
+
+  def set_portfolio
+
+  end
+
+  def update
+    @portfolios_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolios_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: "Portfolio was successfully updated." }
+        format.json { render :show, status: :ok, location: @blog }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @portfolios_item.errors, status: :unprocessable_entity }
+      end
     end
+  end
+
 end
