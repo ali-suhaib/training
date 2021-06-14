e808dafe464effee4046aee2b775ad22d3df92fc
alisuhaib1512 <alisuhaib88@gmail.com>
2021-06-14 13:37:45 +0500

    Customized registration login and log out routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,26 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  ## Devise
+
+  devise_for :users, path:'', path_names: { sign_in: 'login',sign_out: 'logout' , sign_up: "register" }
+
+  ## Portfolios
+
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+
+  ## Pages
 
   get 'about-me', to: 'pages#about'
   get 'pages/about'
   get 'contact', to: 'pages#contact'
+
+  ## blogs
+
   resources :blogs
 
+  ## Root
+
   root to: 'pages#home'
+
 end
